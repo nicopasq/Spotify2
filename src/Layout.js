02c7fec91b4c44207f7b1078f344a0784f692a45
Nico <nicopasquariello@gmail.com>
2024-03-14 15:11:25 -0600

    added Links componenet

--- src/Layout.js
+++ src/Layout.js
@@ -1,15 +1,24 @@
 import React from "react";
 import './App.css';
+import Links from "./components/Links";
 
 export default function Layout(){
     return (
         <div id='appLayout'>
             <div id="navContainer">
-            <div id="sideBar1"></div>
-            <div id="sideBar2"></div>
+            <div id="sideBar1">
+                <Links/>
             </div>
-            <div id="mainDisplay"></div>
-            <div id="playbackController"></div>
+            <div id="sideBar2">
+                {/* <Media/> */}
+            </div>
+            </div>
+            <div id="mainDisplay">
+                {/* <Display/> */}
+            </div>
+            <div id="playbackController">
+                {/* <PlaybackControls/> */}
+            </div>
         </div>
     )
 }
---
+++ src/components/Links.js
@@ -0,0 +1,20 @@
+import { Link, Paper } from "@mui/material";
+import React from "react";
+import '../App.css'
+
+export default function Links(){
+    return (
+        <Paper elevation={4} id="linkNav">
+    <div className="linkNavButton">
+        <Link href={'/'}>
+            Profile
+        </Link>
+    </div>
+    <div className="linkNavButton">
+        <Link href={'/search'}>
+            Search
+        </Link>
+    </div>
+    </Paper>
+    )
+}
